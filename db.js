40e6eb260d08bb33cb336dd6bf372654f481e56f
unknown <ronaksahu0204@gmail.com>
2020-11-04 10:50:18 +0530

    removed prod check for env

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
-if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
-}
+
 const mongoose = require('mongoose');
 
 const options = {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
-}
+
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
